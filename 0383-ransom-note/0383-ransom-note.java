0644397aecd0ad9690dbdef80728654a796dc73d
Saakhivashisth <saakhi.vashisth@gmail.com>
2026-04-15 17:25:14 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,20 +1,20 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        HashMap<Character,Integer> map=new HashMap<>();
-        for(char c:ransomNote.toCharArray()){
-            map.put(c,map.getOrDefault(c,0)+1);
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (char c : ransomNote.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
         }
 
-        for(char ch:magazine.toCharArray()){
-            if(map.containsKey(ch)){
-            map.put(ch,map.get(ch)-1);
-             if(map.get(ch)==0){
-                map.remove(ch);
+        for (char ch : magazine.toCharArray()) {
+            if (map.containsKey(ch)) {
+                map.put(ch, map.get(ch) - 1);
+                if (map.get(ch) == 0) {
+                    map.remove(ch);
+                }
             }
-            }
-           
+
         }
         return map.isEmpty();
-        
+
     }
 }
